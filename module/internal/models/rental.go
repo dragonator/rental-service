ab5de8e093b28c18b20c2498bb862940643829a1
Teodor Draganov <teodor.draganov@theopentag.com>
2023-07-13 18:34:34 +0300

    Add models

---
+++ module/internal/models/rental.go
@@ -0,0 +1,29 @@
+package models
+
+import "time"
+
+// Rental - a model for the rental entity.
+type Rental struct {
+	ID              int32
+	UserID          int32
+	Name            string
+	Type            string
+	Description     string
+	Sleeps          int32
+	PricePerDay     int64
+	HomeCity        string
+	HomeState       string
+	HomeZip         string
+	HomeCountry     string
+	VehicleMake     string
+	VehicleModel    string
+	VehicleYear     int32
+	VehicleLength   float32
+	Latitude        float32
+	Longitude       float32
+	PrimaryImageURL string
+	Created         time.Time
+	Updated         time.Time
+
+	User *User
+}
---
+++ module/internal/models/user.go
@@ -0,0 +1,8 @@
+package models
+
+// User - a model for the user entity.
+type User struct {
+	ID        int
+	FirstName string
+	LastName  string
+}
